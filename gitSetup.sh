719e269a43156c704944f0e9684892223f2a077e
Mike Fugate <mfugate@carnegielearning.com>
2020-03-16 19:02:36 -0400

    only initialize conf git repo if it hasn't been already done

--- gitSetup.sh
+++ gitSetup.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+CONF=/conf
+
 GIT_REPO_URL=https://github.com/mfugate1/appdaemon.git
 GIT_BRANCH=master
 
-cd /conf
-git init
-git remote add origin ${GIT_REPO_URL}
-git fetch
-git checkout -t origin/${GIT_BRANCH}
-cd -
+if [ ! -d $CONF/.git ]; then
+    cd /conf
+    git init
+    git remote add origin ${GIT_REPO_URL}
+    git fetch
+    git checkout -t origin/${GIT_BRANCH}
+    cd -
+fi
 
 ./dockerStart.sh "$@"
