9df1d405a2543d4436edc0cbed07d5752d87fa3d
Mike Fugate <mfugate@carnegielearning.com>
2020-03-16 19:12:14 -0400

    add app that can pull latest changes into conf

---
+++ apps/ConfUpdater/ConfUpdater.py
@@ -0,0 +1,14 @@
+import hassapi as hass
+import subprocess
+
+class ConfUpdater(hass.Hass):
+
+    def initialize(self):
+        self.run_event_listener = self.listen_event(self.run_updater, self.args.get('event', 'RUN_CONF_UPDATER'))
+        
+    def terminate(self):
+        if hasattr(self, 'run_event_listener'):
+            self.cancel_timer(self.run_event_listener)
+            
+    def run_updater(self, event_name, data, kwargs):
+        subprocess.Popen(['/usr/bin/git', 'pull'], cwd = '/conf')
