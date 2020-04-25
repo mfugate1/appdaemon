a5c04d0e7fc73d0a10186b30ef1242c4d2cb8445
Mike Fugate <mfugate@carnegielearning.com>
2020-04-25 15:41:39 -0400

    handle turning on receivers, tvs, and media sources

--- apps/MediaController/MediaController.py
+++ apps/MediaController/MediaController.py
@@ -18,9 +18,48 @@ class MediaController(hass.Hass):
         if not room:
             self.log("[media_controller_command] No room given or found with alexa last_called")
             return
-            
-        self.log("[media_controller_command] Room = {}".format(room))
 
+        if 'source' not in data:
+            self.log("[media_controller_command] Missing source, nothing to do!")
+            return
+            
+        source = data['source']
+        room_info = self.args['rooms'][room]
+            
+        if source not in room_info['sources']:
+            self.log("[media_controller_command] {} is not a source in {}".format(source, room))
+            return
+        
+        self.log("[media_controller_command] Room = {}, Source = {}".format(room, data['source']))
+        
+        source_info = room_info['sources'][source]
+        
+        if 'receiver_source' in source_info:
+            self.run_in(self.media_player_on, 0, entity_id = room_info['receiver'], source = source_info['receiver_source'])
+            
+        if 'tv_source' in source_info:
+            self.run_in(self.media_player_on, 0, entity_id = room_info['tv'], source = source_info['tv_source'])
+            
+        if 'media' in source_info:
+            media = source_info['media']
+            if 'data' in media:
+                media_data = media['data']
+            else:
+                media_data = {}
+                
+            self.call_service(media['service'], entity_id = media['entity_id'], **media_data)
+
+    def media_player_on(self, kwargs):
+        delay = 0
+        if self.get_state(kwargs['entity_id']) == 'off':
+            self.turn_on(kwargs['entity_id'])
+            delay = 4
+            
+        self.run_in(self.media_player_set_source, delay, **kwargs)
+            
+    def media_player_set_source(self, kwargs):
+        self.call_service('media_player/select_source', **kwargs)
+    
     def get_last_used_alexa(self):
         for room, values in self.args['rooms'].items():
             if self.get_state(values['alexa'], attribute='last_called'):
