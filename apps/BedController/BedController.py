7b1d77b84f681c9655b56c41d48f9fbc7df74423
Mike Fugate <mfugate@carnegielearning.com>
2020-05-11 14:50:14 -0400

    stop listeners when doing other stuff. poll every 10 seconds

--- apps/BedController/BedController.py
+++ apps/BedController/BedController.py
@@ -17,44 +17,23 @@ class BedController(hass.Hass):
 
     def initialize(self):
         self.client = self.get_client()
-        self.run_in(self.update, 0)
-        self.update_timer = self.run_every(self.update, datetime.now(), self.args['update_interval_seconds'])
-        self.stop_motion_listener = self.listen_event(self.stop_motion, self.args['stop_motion_event'])
+        self.start_update_timer()
         self.start_state_listeners()
+        self.run_in(self.update, 0)
+        self.stop_motion_listener = self.listen_event(self.stop_motion, self.args['stop_motion_event'])
         
     def terminate(self):
         self.stop_state_listeners()
-        if hasattr(self, 'update_timer'):
-            self.cancel_timer(self.update_timer)
-    
-    def get_client(self):
-        client = Sleepyq(secrets.SLEEP_IQ_USER, secrets.SLEEP_IQ_PASSWORD)
-        client.login()
-        return client
-        
-    def preset(self, entity, attribute, old, new, kwargs):
-        self.log('[preset] Setting bed to preset position {}'.format(new))
-        self.client.preset(BED_PRESETS[new], 'r')
-    
-    def set_position(self, entity, attribute, old, new, kwargs):
-        self.log('[set_position] Setting bed position, actuator = {}, position = {}'.format(kwargs['actuator'], new))
-        self.client.set_foundation_position('r', kwargs['actuator'], float(new))
-    
-    def set_sleepnumber(self, entity, attribute, old, new, kwargs):
-        self.log('[set_sleepnumber] Setting bed sleep number, side = {}, value = {}'.format(kwargs['side'], new))
-        self.client.set_sleepnumber(kwargs['side'], float(new))
-    
+        self.cancel_update_timer()
+            
     def start_state_listeners(self):
         self.preset_listener = self.listen_state(self.preset, self.args['preset_entity'])
         self.set_foot_position_listener = self.listen_state(self.set_position, self.args['foot_position_entity'], actuator = 'f')
         self.set_head_position_listener = self.listen_state(self.set_position, self.args['head_position_entity'], actuator = 'h')
         self.set_left_sleepnumber_listener = self.listen_state(self.set_sleepnumber, self.args['left_sleep_number_entity'], side = 'l')
         self.set_right_sleepnumber_listener = self.listen_state(self.set_sleepnumber, self.args['right_sleep_number_entity'], side = 'r')
-    
-    def stop_motion(self, event_name, data, kwargs):
-        self.client.stop_motion('r')
         
-    def stop_state_listeners(self):
+    def cancel_state_listeners(self):
         if hasattr(self, 'preset_listener'):
             self.cancel_listen_state(self.preset_listener)
         if hasattr(self, 'set_foot_position_listener'):
@@ -65,9 +44,44 @@ class BedController(hass.Hass):
             self.cancel_listen_state(self.set_left_sleepnumber_listener)
         if hasattr(self, 'set_right_sleepnumber_listener'):
             self.cancel_listen_state(self.set_right_sleepnumber_listener)
+            
+    def start_update_timer(self):
+        self.update_timer = self.run_every(self.update, datetime.now(), self.args['update_interval_seconds'])
+        
+    def cancel_update_timer(self):
+        if hasattr(self, 'update_timer'):
+            self.cancel_timer(self.update_timer)
+    
+    def get_client(self):
+        client = Sleepyq(secrets.SLEEP_IQ_USER, secrets.SLEEP_IQ_PASSWORD)
+        client.login()
+        return client
+        
+    def preset(self, entity, attribute, old, new, kwargs):
+        self.log('[preset] Setting bed to preset position {}'.format(new))
+        self.cancel_update_timer()
+        self.client.preset(BED_PRESETS[new], 'r')
+        self.start_update_timer()
+    
+    def set_position(self, entity, attribute, old, new, kwargs):
+        self.log('[set_position] Setting bed position, actuator = {}, position = {}'.format(kwargs['actuator'], new))
+        self.cancel_update_timer()
+        self.client.set_foundation_position('r', kwargs['actuator'], float(new))
+        self.start_update_timer()
+    
+    def set_sleepnumber(self, entity, attribute, old, new, kwargs):
+        self.log('[set_sleepnumber] Setting bed sleep number, side = {}, value = {}'.format(kwargs['side'], new))
+        self.cancel_update_timer()
+        self.client.set_sleepnumber(kwargs['side'], float(new))
+        self.start_update_timer()
+        
+    def stop_motion(self, event_name, data, kwargs):
+        self.client.stop_motion('r')
         
     def update(self, kwargs):
         foundationStatus = self.client.foundation_status()
+        
+        self.stop_state_listeners()
         
         if foundationStatus.fsCurrentPositionPresetRight != 'Not at preset':
             self.select_option(self.args['preset_entity'], foundationStatus.fsCurrentPositionPresetRight)
@@ -79,3 +93,5 @@ class BedController(hass.Hass):
         
         self.set_value(self.args['left_sleep_number_entity'], sleeperStatus.left.sleepNumber)
         self.set_value(self.args['right_sleep_number_entity'], sleeperStatus.right.sleepNumber)
+        
+        self.start_state_listeners()
