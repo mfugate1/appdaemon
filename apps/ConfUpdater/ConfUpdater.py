0ba08f33cc94ffe9b911a6c3fb292dba42c7df42
Mike Fugate <mfugate@carnegielearning.com>
2020-05-11 14:54:59 -0400

    split git pull and sleepyq update into separate events

--- apps/ConfUpdater/ConfUpdater.py
+++ apps/ConfUpdater/ConfUpdater.py
@@ -4,13 +4,19 @@ import subprocess
 class ConfUpdater(hass.Hass):
 
     def initialize(self):
-        self.run_event_listener = self.listen_event(self.run_updater, self.args.get('event', 'RUN_CONF_UPDATER'))
+        self.git_pull_event_listener = self.listen_event(self.git_pull, self.args['git_pull_event'])
+        self.update_sleepyq_event_listener = self.listen_event(self.update_sleepyq, self.args['update_sleepyq_event'])
         
     def terminate(self):
-        if hasattr(self, 'run_event_listener'):
-            self.cancel_timer(self.run_event_listener)
+        if hasattr(self, 'git_pull_event_listener'):
+            self.cancel_timer(self.git_pull_event_listener)
+        if hasattr(self, 'update_sleepyq_event_listener'):
+            self.cancel_timer(self.update_sleepyq_event_listener)
             
-    def run_updater(self, event_name, data, kwargs):
-        self.log("[run_updater] Pulling latest configuration")
+    def git_pull(self, event_name, data, kwargs):
+        self.log("[git_pull] Pulling latest configuration")
         subprocess.Popen(['/usr/bin/git', 'pull'], cwd = '/conf')
+        
+    def update_sleepyq(self, event_name, data, kwargs):
+        self.log("[update_sleepyq] Reinstalling mfugate1/sleepyq python module")
         subprocess.Popen(['/usr/local/bin/pip3', 'install', '-e', 'git+https://github.com/mfugate1/sleepyq.git#egg=sleepyq'])
